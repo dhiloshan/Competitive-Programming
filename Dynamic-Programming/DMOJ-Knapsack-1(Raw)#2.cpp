aa7315eb6925b514cca127445706d6ce59aa758c
Dhiloshan Sayanthan <dhiloshan@gmail.com>
2024-05-12 14:32:19 -0400

    Create DMOJ-Knapsack-1(Raw)#2.cpp
    Solves the problem through a 2 row flip array

---
+++ Dynamic-Programming/DMOJ-Knapsack-1(Raw)#2.cpp
@@ -0,0 +1,20 @@
+// SOLUTION 2: 2 Row Flip DP ARRAY (2 by W)
+// RAW CODE - https://dmoj.ca/problem/dpd
+// Educational DP Contest AtCoder D Knapsack 1 (DMOJ) - https://dmoj.ca/src/6397530
+#include <bits/stdc++.h>
+using namespace std;
+const int MM = 1e5+2;
+int N, W; long long dp[3][MM];
+int main(){
+    cin >> N >> W;
+    for(int i = 1, v, w; i <= N; i++){
+        cin >> w >> v;
+        for(int j = 1; j <= W; j++){
+            dp[1][j] = (w > j) ? dp[0][j] : max(dp[0][j], v + dp[0][j-w]);
+        }
+        for(int k = 1; k <= W; k++){
+            dp[0][k] = dp[1][k];
+        }
+    }
+    cout << dp[1][W];
+}
