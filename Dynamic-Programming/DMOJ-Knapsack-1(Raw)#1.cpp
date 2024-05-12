7412cacfe3db7975f474218b037dd81f554e241d
Dhiloshan Sayanthan <dhiloshan@gmail.com>
2024-05-12 11:18:43 -0400

    Updated the dmoj code url in DMOJ-Knapsack-1(Raw)#1.cpp

--- Dynamic-Programming/DMOJ-Knapsack-1(Raw)#1.cpp
+++ Dynamic-Programming/DMOJ-Knapsack-1(Raw)#1.cpp
@@ -1,6 +1,6 @@
 // SOLUTION 1: STANDARD TEMPLATE: 2D DP ARRAY (N by W)
 // RAW CODE - https://dmoj.ca/problem/dpd
-// Educational DP Contest AtCoder D Knapsack 1 (DMOJ) - https://dmoj.ca/src/6396236
+// Educational DP Contest AtCoder D Knapsack 1 (DMOJ) - https://dmoj.ca/src/6397130
 #include <bits/stdc++.h>
 using namespace std;
 const int MM = 1e5+2;
