4102863a50dad9bf3d479c9d4eed0df643d58336
Dhiloshan Sayanthan <dhiloshan@gmail.com>
2024-05-15 16:42:33 -0400

    Update DMOJ-BIT-Test(Raw).cpp

--- Fenwick-Tree/DMOJ-BIT-Test(Raw).cpp
+++ Fenwick-Tree/DMOJ-BIT-Test(Raw).cpp
@@ -1,5 +1,5 @@
-// RAW CODE - https://dmoj.ca/src/6342254
 // Binary Indexed Tree Test (DMOJ) - https://dmoj.ca/problem/ds1
+// RAW CODE - https://dmoj.ca/src/6342254
 #include <bits/stdc++.h> 
 using namespace std;
 typedef long long ll;
