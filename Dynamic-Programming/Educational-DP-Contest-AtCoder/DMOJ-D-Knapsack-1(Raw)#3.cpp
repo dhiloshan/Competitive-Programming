c51bb0714edf5a84f5702fc621f0c610c356adef
Dhiloshan Sayanthan <dhiloshan@gmail.com>
2024-06-20 23:11:09 -0400

    Update DMOJ-D-Knapsack-1(Raw)#3.cpp

--- Dynamic-Programming/Educational-DP-Contest-AtCoder/DMOJ-D-Knapsack-1(Raw)#3.cpp
+++ Dynamic-Programming/Educational-DP-Contest-AtCoder/DMOJ-D-Knapsack-1(Raw)#3.cpp
@@ -1,5 +1,5 @@
 // SOLUTION 3: 1D DP ARRAY (size W)
-// Educational DP Contest AtCoder D Knapsack 1 (DMOJ) - https://dmoj.ca/problem/dpd
+// Educational DP Contest AtCoder D Knapsack 1 (DMOJ): https://dmoj.ca/problem/dpd
 // RAW CODE - https://dmoj.ca/src/6400062
 #include <bits/stdc++.h>
 using namespace std;
