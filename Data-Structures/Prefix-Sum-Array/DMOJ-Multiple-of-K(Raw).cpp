056324ef730c70f7eb7bd4c9ae5bfba053dd3405
Dhiloshan Sayanthan <dhiloshan@gmail.com>
2024-06-24 14:43:38 -0400

    updated the solution with a more efficient solution

--- Data-Structures/Prefix-Sum-Array/DMOJ-Multiple-of-K(Raw).cpp
+++ Data-Structures/Prefix-Sum-Array/DMOJ-Multiple-of-K(Raw).cpp
@@ -3,7 +3,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 const int MM = 1e6+3;
-int N, K, psa[MM], ans=-1; map<int, int> rem;
+int N, K, psa[MM], rem[MM], ans=-1;
 int main() {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     cin >> N >> K;
@@ -11,12 +11,15 @@ int main() {
     for(int i = 2, x; i <= N; i++){
         cin >> x; psa[i] = (psa[i-1] + x) % K;
     }
+    fill(rem, rem+MM, -1);
     rem[0] = 0;
     for(int i = 1; i <= N; i++){
-        if(rem.count(psa[i]))
-             ans = max(ans, i - rem[psa[i]]);
-        else
+        if(rem[psa[i]] != -1){
+            ans = max(ans, i - rem[psa[i]]);
+        }
+        else{
             rem[psa[i]] = i;
+        }
     }
     cout << ans << endl;
 }
