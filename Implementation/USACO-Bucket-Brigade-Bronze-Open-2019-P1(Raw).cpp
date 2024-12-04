35af378541c51251b391a4d308cdefdd42a5f6fe
Dhiloshan Sayanthan <dhiloshan@gmail.com>
2024-12-04 12:18:23 -0500

    Update USACO-Bucket-Brigade-Bronze-Open-2019-P1(Raw).cpp

--- Implementation/USACO-Bucket-Brigade-Bronze-Open-2019-P1(Raw).cpp
+++ Implementation/USACO-Bucket-Brigade-Bronze-Open-2019-P1(Raw).cpp
@@ -7,7 +7,6 @@ int main(){
     ios::sync_with_stdio(0); cin.tie(0);
     // ifstream cin("buckets.in");
     // ofstream cout("buckets.out");
-    // include the above two lines when submitting on usaco.org as the judge requires an output file
     for(int i = 0; i < 10; i++){
         for(int j = 0; j < 10; j++){
             char c; cin >> c;
