1aba3734d47da2a25ad57e240e9c9b3d810f8380
Dhiloshan Sayanthan <dhiloshan@gmail.com>
2024-12-04 14:12:45 -0500

    Update USACO-Bucket-Brigade-Bronze-Open-2019-P1(Annotated).cpp

--- Implementation/USACO-Bucket-Brigade-Bronze-Open-2019-P1(Annotated).cpp
+++ Implementation/USACO-Bucket-Brigade-Bronze-Open-2019-P1(Annotated).cpp
@@ -8,7 +8,7 @@ int main(){
     // ifstream cin("buckets.in");
     // ofstream cout("buckets.out");
     // include the above two lines when submitting on usaco.org as the judge requires an output file
-    for(int i = 0; i < 10; i++){
+    for(int i = 0; i < 10; i++){ // 10 by 10 array
         for(int j = 0; j < 10; j++){
             char c; cin >> c;
             if(c == 'B'){
@@ -22,9 +22,10 @@ int main(){
             }
         }
     }
-    int min = abs(lx-bx)+abs(ly-by)-1;
+    int min = abs(lx-bx)+abs(ly-by)-1; // if the rock doesn't interrupt the path this is the minimum distance
     if((bx == lx && bx == rx && ((by < ry && ry < ly) || (ly < ry && ry < by))) || (by == ly && by == ry && ((bx < rx && rx < lx) || (lx < rx && rx < bx)))){
-        min += 2;
+        // the if statement checks if the rock is in the way, and the only way it is possible is if both the barn and lake is on the same axis and the rock is in-between those two areas
+        min += 2; // if the rock is in the way it will add a cost of 2
     }
     cout << min << endl;
 }
