5e404992cbffa83433fa5a42c9ee2766871b1654
Dhiloshan Sayanthan <dhiloshan@gmail.com>
2024-12-12 11:56:18 -0500

    Create CSES-Counting-Divisors(Annotated).cpp

---
+++ Math/CSES-Counting-Divisors(Annotated).cpp
@@ -0,0 +1,17 @@
+// CSES Problem Set Counting Divisors (CSES): https://cses.fi/problemset/task/1713/
+// ANNOTATED CODE -  https://cses.fi/paste/c45a7db59c7afa35ae1571/
+#include <bits/stdc++.h>
+using namespace std;
+int N;
+int main(){
+    cin >> N;
+    for(int i = 0, x; i < N; i++){
+        cin >> x;
+        int cnt = 0;
+        for(int i = 1; i * i <= x; i++){ // runs in square root N time
+            if(i * i == x) cnt++; // ex. 2 * 2 == 4, so there is only one divisor
+            else if(x % i == 0) cnt+=2; // 1 * 4 == 4, so there are two divisors
+        }
+        cout << cnt << endl; // answer queries right away, no need to store it
+    }
+}
