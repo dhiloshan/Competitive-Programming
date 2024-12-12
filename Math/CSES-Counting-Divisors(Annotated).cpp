977041cab3401c68e70fea861787fbf88963942d
Dhiloshan Sayanthan <dhiloshan@gmail.com>
2024-12-12 11:57:08 -0500

    Update CSES-Counting-Divisors(Annotated).cpp

--- Math/CSES-Counting-Divisors(Annotated).cpp
+++ Math/CSES-Counting-Divisors(Annotated).cpp
@@ -8,10 +8,10 @@ int main(){
     for(int i = 0, x; i < N; i++){
         cin >> x;
         int cnt = 0;
-        for(int i = 1; i * i <= x; i++){ // runs in square root N time
+        for(int i = 1; i * i <= x; i++){ // runs in square root x time
             if(i * i == x) cnt++; // ex. 2 * 2 == 4, so there is only one divisor
             else if(x % i == 0) cnt+=2; // 1 * 4 == 4, so there are two divisors
         }
         cout << cnt << endl; // answer queries right away, no need to store it
-    }
+    } // O(Nsqrt(x))
 }
