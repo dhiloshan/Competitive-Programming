bf383480aee4e83fd715c5d9eb2ca1b0b414bcc3
Dhiloshan Sayanthan <dhiloshan@gmail.com>
2024-12-31 12:35:20 -0500

    Update CCC-RoboThieves-2018-S3(Raw).cpp

--- Graph-Theory/Shortest-Path/CCC-RoboThieves-2018-S3(Raw).cpp
+++ Graph-Theory/Shortest-Path/CCC-RoboThieves-2018-S3(Raw).cpp
@@ -1,48 +1,55 @@
 // CCC '18 S3 - RoboThieves (DMOJ): https://dmoj.ca/problem/ccc18s3
-// RAW CODE - https://dmoj.ca/src/6846379
+// RAW CODE - https://dmoj.ca/src/6846647
 #include <bits/stdc++.h>
 using namespace std;
 const int MM = 102;
 typedef pair<int, int> pi;
-int N, M, dir[4][2]={{-1, 0}, {0, 1}, {1, 0}, {0, -1}}; char g[MM][MM]; bool vis[MM][MM]; pi st; vector<pi> cam;
-int dis[MM][MM]; priority_queue<array<int, 3>> q;
-void push(int r, int c, int d){
-    if(g[r][c] != 'W' && !vis[r][c] && dis[r][c] > d) {
-            q.push({-d, r, c}); dis[r][c] = d;
+typedef pair<int, pi> pii; 
+int N, M, dis[MM][MM]; bool vis[MM][MM]; char g[MM][MM]; vector<pi> cam; priority_queue<pii, vector<pii>, greater<> > q; pi st;
+int d[4][2] = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+void push(int w, int r, int c){ 
+    if(g[r][c] != 'W' && !vis[r][c] && dis[r][c] > w){
+        q.push({w, {r, c}}); dis[r][c] = w; 
     }
 }
 int main(){
     cin >> N >> M;
-    for(int i=1; i<=N; i++){
-        for(int j=1; j<=M; j++){
+    for(int i = 1; i <= N; i++){
+        for(int j = 1; j <= M; j++){
             cin >> g[i][j];
-            if(g[i][j] == 'S') st = {i, j};
-            if(g[i][j] == 'C') cam.push_back({i, j});
+            if(g[i][j] == 'S'){ st.first = i; st.second = j; g[i][j] = '.'; } 
+            else if(g[i][j] == 'C'){ cam.emplace_back(i, j); vis[i][j] = true; } 
         }
     }
-    for(auto e: cam){
-        for(int k=0; k<4; k++){
-            int r = e.first, c = e.second;
-            while(g[r][c] != 'W'){
-                if(g[r][c]=='.' || g[r][c]=='S') vis[r][c] = 1;
-                r += dir[k][0]; c += dir[k][1];
+    for(auto [r, c] : cam){
+        for(int k = 0; k < 4; k++){
+            int nr = r + d[k][0], nc = c + d[k][1]; 
+            while(nr > 1 && nr < N && nc > 1 && nc < M && g[nr][nc] != 'W'){ 
+                if(g[nr][nc] == '.') vis[nr][nc] = true; 
+                nr += d[k][0]; nc += d[k][1]; 
             }
         }
     }
-    memset(dis, 0x3f, sizeof(dis));
-    if(!vis[st.first][st.second]) { q.push({0, st.first, st.second});  dis[st.first][st.second] = 0;}
-    while(!q.empty()){
-        auto [d, r, c] = q.top(); q.pop();  d=-d;
-        if(d > dis[r][c]) continue;
-        if(g[r][c] == '.' || g[r][c] == 'S'){
-            for(int k=0; k<4; k++)
-                push(r + dir[k][0], c + dir[k][1], dis[r][c]+1);
-        }else if(g[r][c] == 'L') push(r, c-1, dis[r][c]);
-        else if(g[r][c] == 'R') push(r, c+1, dis[r][c]);
-        else if(g[r][c] == 'U') push(r-1, c, dis[r][c]);
-        else if(g[r][c] == 'D') push(r+1, c, dis[r][c]);
+    memset(dis, 0x3f, sizeof(dis)); 
+    if(!vis[st.first][st.second]){
+        dis[st.first][st.second] = 0; q.push({0, {st.first, st.second}}); 
     }
-    for(int i=1; i<=N; i++)
-        for(int j=1; j<=M; j++)
-            if(g[i][j] == '.') cout << (dis[i][j]==0x3f3f3f3f? -1: dis[i][j]) << "\n";
+    while(!q.empty()){
+        auto [w, pos]  = q.top(); q.pop();
+        int r = pos.first, c = pos.second;
+        if(w > dis[r][c]) continue; 
+        if(g[r][c] == '.')
+            for(int k = 0; k < 4; k++) 
+                push(dis[r][c] + 1, r + d[k][0], c + d[k][1]);
+        else if(g[r][c] == 'L') push(dis[r][c], r, c-1); 
+        else if(g[r][c] == 'R') push(dis[r][c], r, c+1); 
+        else if(g[r][c] == 'U') push(dis[r][c], r-1, c);
+        else if(g[r][c] == 'D') push(dis[r][c], r+1, c);
+    }
+    for(int i = 1; i <= N; i++){
+        for(int j = 1; j <= M; j++){
+            if(g[i][j] == '.' && !(i == st.first && j == st.second)) 
+                cout << (dis[i][j]==0x3f3f3f3f? -1 : dis[i][j]) << endl;
+        }
+    }
 }
