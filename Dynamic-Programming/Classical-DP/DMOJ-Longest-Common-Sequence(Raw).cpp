4db293886738a3939a48af9b7cd90c2408958ade
Dhiloshan Sayanthan <dhiloshan@gmail.com>
2025-01-09 12:44:14 -0500

    Create DMOJ-Longest-Common-Sequence(Raw).cpp

---
+++ Dynamic-Programming/Classical-DP/DMOJ-Longest-Common-Sequence(Raw).cpp
@@ -0,0 +1,20 @@
+// Longest Common Subsequence (DMOJ): https://dmoj.ca/problem/lcs
+// RAW CODE - https://dmoj.ca/src/6872218
+#include <bits/stdc++.h>
+using namespace std;
+const int MM = 1e3+3;
+typedef long long ll;
+int N, M, a[MM], b[MM]; ll dp[MM][MM];
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cin >> N >> M;
+    for(int i = 1; i <= N; i++) cin >> a[i];
+    for(int i = 1; i <= M; i++) cin >> b[i];
+    for(int i = 1; i <= N; i++){
+        for(int j = 1; j <= M; j++){
+            if(a[i] == b[j]) dp[i][j] = dp[i-1][j-1] + 1;
+            else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+        }
+    }
+    cout << dp[N][M] << endl;
+}
