af7a84a0733a8e43b00d3f896acf18ba3e783f93
Dhiloshan Sayanthan <dhiloshan@gmail.com>
2025-01-19 00:28:25 -0500

    Create CCC-Bowling-For-Numbers-2007-S5(Raw).cpp

---
+++ Dynamic-Programming/Classical-DP/CCC-Bowling-For-Numbers-2007-S5(Raw).cpp
@@ -0,0 +1,22 @@
+// CCC '07 S5 - Bowling for Numbers (DMOJ): https://dmoj.ca/problem/ccc07s5
+// RAW CODE
+#include <bits/stdc++.h>
+using namespace std;
+const int MM = 3e4+5;
+int n, k, w, t, dp[502][MM], psa[MM];
+
+int main(){
+    for(cin >> t; t > 0; t--){
+        cin >> n >> k >> w;
+        for(int i = 1, x; i <= n; i++){
+            cin >> x; psa[i] = psa[i-1] + x;
+        }
+        for(int i = 1; i <= k; i++){
+            for(int j = 1; j <= n; j++){
+                if(j < w) dp[i][j] = psa[j];
+                else dp[i][j] = max(dp[i][j-1], psa[j] - psa[j-w] + dp[i-1][j-w]);
+            }
+        }
+        cout << dp[k][n] << endl;
+    }
+}
