29bceae7c6ece23eaf497898a0c40d0deeaa047b
Dhiloshan Sayanthan <dhiloshan@gmail.com>
2025-01-19 00:28:44 -0500

    Update CCC-Bowling-For-Numbers-2007-S5(Raw).cpp

--- Dynamic-Programming/Classical-DP/CCC-Bowling-For-Numbers-2007-S5(Raw).cpp
+++ Dynamic-Programming/Classical-DP/CCC-Bowling-For-Numbers-2007-S5(Raw).cpp
@@ -1,10 +1,9 @@
 // CCC '07 S5 - Bowling for Numbers (DMOJ): https://dmoj.ca/problem/ccc07s5
-// RAW CODE
+// RAW CODE - https://dmoj.ca/src/6899371
 #include <bits/stdc++.h>
 using namespace std;
 const int MM = 3e4+5;
 int n, k, w, t, dp[502][MM], psa[MM];
-
 int main(){
     for(cin >> t; t > 0; t--){
         cin >> n >> k >> w;
