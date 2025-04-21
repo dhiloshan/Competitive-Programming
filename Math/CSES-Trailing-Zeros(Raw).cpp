51112e7d35e347c8b99c958cc00fa8a55112fcf3
Dhiloshan Sayanthan <dhiloshan@gmail.com>
2025-04-21 11:33:01 -0400

    Update CSES-Trailing-Zeros(Raw).cpp

--- Math/CSES-Trailing-Zeros(Raw).cpp
+++ Math/CSES-Trailing-Zeros(Raw).cpp
@@ -1,5 +1,5 @@
 // CSES Problem Set Trailing Zeros (CSES): https://cses.fi/problemset/task/1618
-// RAW CODE
+// RAW CODE - https://cses.fi/paste/11a1f324b38be3bbc1b2c0/
 #include <bits/stdc++.h>
 using namespace std;
 int N, two, five;
