bb3513230d0bb0ef2e99688c77c8c836d6d05000
Dhiloshan Sayanthan <dhiloshan@gmail.com>
2025-05-04 12:45:50 -0400

    Create Leetcode-785-Is-Graph-Bipartite(Raw).cpp

---
+++ Graph-Theory/Basic-Traversal/Leetcode-785-Is-Graph-Bipartite(Raw).cpp
@@ -0,0 +1,28 @@
+// Problem 785. Is Graph Bipartite? (Leetcode) : https://leetcode.com/problems/is-graph-bipartite/
+// RAW CODE
+// SOLUTION #1: BFS
+class Solution {
+public:
+    bool isBipartite(vector<vector<int>>& graph) {
+        bool col[101], vis[101]; 
+        memset(col, false, sizeof(col)); memset(vis, false, sizeof(vis));
+        queue<int> q; 
+        for(int i = 0; i < graph.size(); i++){ 
+            if(!vis[i]){
+                q.push(i); vis[i] = true;
+                while(!q.empty()){
+                    int cur = q.front(); q.pop();
+                    for(int nxt : graph[cur]){
+                        if(!vis[nxt]){
+                            vis[nxt] = true; q.push(nxt); col[nxt] = !col[cur];
+                        }
+                        else{
+                            if(col[nxt] == col[cur]) return false;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+};
