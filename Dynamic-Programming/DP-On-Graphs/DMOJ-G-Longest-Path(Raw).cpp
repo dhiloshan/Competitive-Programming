7cdc99b4fff5cb3d8f02aeff02df3c9e44136878
Dhiloshan Sayanthan <dhiloshan@gmail.com>
2025-07-26 00:00:24 -0400

    Create DMOJ-G-Longest-Path(Raw).cpp

---
+++ Dynamic-Programming/DP-On-Graphs/DMOJ-G-Longest-Path(Raw).cpp
@@ -0,0 +1,43 @@
+// Educational DP Contest AtCoder G - Longest Path (DMOJ): https://dmoj.ca/problem/dpg
+// RAW CODE
+#include <bits/stdc++.h>
+using namespace std;
+const int MM = 1e5+5;
+int N, M, in[MM], dp[MM];
+vector<int> adj[MM], rev[MM];
+vector<int> order;
+int main(){
+    cin >> N >> M;
+    for(int i = 0, u, v; i < M; i++){
+        cin >> u >> v;
+        adj[u].push_back(v);
+        rev[v].push_back(u);
+        in[v]++;
+    }
+    queue<int> q;
+    for(int i = 1; i <= N; i++){
+        if(in[i] == 0){
+            q.push(i);
+            dp[i] = 0;
+        }
+    }
+    while(!q.empty()){
+        int cur = q.front(); q.pop();
+        for(int nxt : adj[cur]){
+            if(--in[nxt] == 0){
+                q.push(nxt);
+                order.push_back(nxt);
+            }
+        }
+    }
+    int mx = 0;
+    for(int node : order) {
+        int curMx = 0;
+        for(int opt : rev[node]){
+            curMx = max(curMx, dp[opt] + 1);
+        }
+        dp[node] = curMx;
+        mx = max(mx, curMx);
+    }
+    cout << mx << endl;
+}
