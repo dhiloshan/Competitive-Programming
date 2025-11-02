9e5523e75933d374f139c96eabdf2015027e1338
Dhiloshan Sayanthan <dhiloshan@gmail.com>
2025-11-02 15:12:41 -0500

    Create CCC-2018-J3-Are-we-there-yet(Raw).cpp

---
+++ Implementation/CCC-2018-J3-Are-we-there-yet(Raw).cpp
@@ -0,0 +1,21 @@
+// CCC '18 J3 - Are we there yet? (DMOJ): https://dmoj.ca/problem/ccc18j3
+// RAW CODE
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int a[5], psa[5];
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    for(int i = 1; i <= 4; i++){
+        cin >> a[i]; psa[i] = psa[i-1] + a[i];
+    }
+    for(int i = 0; i <= 4; i++){
+        for(int j = 0; j < i; j++){
+            cout << psa[i] - psa[j] << " ";
+        }
+        for(int k = i; k <= 4; k++){
+            cout << psa[k] - psa[i] << " ";
+        }
+        cout << endl;
+    }
+}
