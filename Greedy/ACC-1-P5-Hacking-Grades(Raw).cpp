3e68bb8bba1987a53dc6a2cee106f503091f5d4f
Dhiloshan Sayanthan <dhiloshan@gmail.com>
2025-11-24 21:24:55 -0500

    Create ACC-1-P5-Hacking-Grades(Raw).cpp

---
+++ Greedy/ACC-1-P5-Hacking-Grades(Raw).cpp
@@ -0,0 +1,41 @@
+// Arcadia Computing Contest 1 P5 - Hacking Grades (DMOJ): https://dmoj.ca/problem/ahscc1p5
+// ANNOTATED CODE
+#include <bits/stdc++.h>
+using namespace std;
+const int MM = 2e5+3;
+const int MOD = 1e9+7;
+typedef pair<int, int> pi;
+typedef long long ll;
+struct e {
+    int n, d, idx;
+    bool operator<(const e &y) const {
+        long double curVal = (long double)(n+1)/(d+1) - (long double)n/d;
+        long double newVal = (long double)(y.n+1)/(y.d+1) - (long double)y.n/y.d;
+        return curVal < newVal;
+    }
+};
+int N, K, numer[MM], denom[MM]; pi ori[MM]; priority_queue<e> q;
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cin >> N >> K;
+    for (int i = 1; i <= N; i++) cin >> numer[i];
+    for (int i = 1; i <= N; i++) cin >> denom[i];
+    for (int i = 1; i <= N; i++) {
+        int n = numer[i], d = denom[i];
+        ori[i] = {n, d};
+        if (n < d) 
+            q.push({n, d, i});
+    }
+    while (!q.empty() && K > 0) {
+        auto [n, d, idx] = q.top(); q.pop();
+        ori[idx] = {++n, ++d};
+        if (n < d)
+            q.push({n, d, idx});
+        K--;
+    }
+    double total = 0;
+    for (int i = 1; i <= N; i++) {
+        total += (((long double)ori[i].first/ori[i].second)*100);
+    }
+    printf("%.9f\n", total/N);
+}
