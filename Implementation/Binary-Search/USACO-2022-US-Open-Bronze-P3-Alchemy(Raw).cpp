e8e2dbdf8dba0907d9fac488fde09b54f1e4b74b
Dhiloshan Sayanthan <dhiloshan@gmail.com>
2025-12-19 20:53:17 -0500

    Create USACO-2022-US-Open-Bronze-P3-Alchemy(Raw).cpp

---
+++ Implementation/Binary-Search/USACO-2022-US-Open-Bronze-P3-Alchemy(Raw).cpp
@@ -0,0 +1,42 @@
+// USACO 2022 US Open, Bronze Problem 3. Alchemy (DMOJ): https://dmoj.ca/problem/usaco22openb3
+// RAW CODE
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int MM = 102;
+int N, K, a[MM]; vector<int> rev[MM];
+bool isValid(int am) {
+    int req[N+1]; memset(req, 0,  sizeof(req));
+    req[N] = am;
+    for (int i = N; i >= 1; i--) {
+        if (req[i] > 1e7) return false; 
+        if (a[i] >= req[i])
+            continue;
+        int rem = req[i] - a[i];
+        if (rev[i].size() == 0)
+            return false;
+        for (int nxt : rev[i]) {
+            req[nxt] += rem;
+        }
+    }
+    return true;
+}
+int main() {
+    cin >> N;
+    for (int i=1; i <= N; i++) cin >> a[i];
+    cin >> K;
+    for (int i = 1, u, m; i <= K; i++) {
+        cin >> u >> m;
+        for (int j = 1, v; j <= m; j++) {
+            cin >> v;
+            rev[u].push_back(v);
+        }
+    }
+    int lo = 0, hi = 1e7;
+    while (hi - lo > 1) {
+        int mid = lo + (hi - lo) / 2;
+        if (isValid(mid)) lo = mid;
+        else hi = mid;
+    }
+    cout << lo << endl;
+}
