e6142aa5c81eb6ee6ff6cb3ad2c8eef29156fc31
Dhiloshan Sayanthan <dhiloshan@gmail.com>
2026-01-02 11:41:44 -0500

    Create Codeforces-D1-Magic-Powder-1-R350-Div2(Annotated)#2.cpp

---
+++ Implementation/Codeforces-D1-Magic-Powder-1-R350-Div2(Annotated)#2.cpp
@@ -0,0 +1,30 @@
+// D1. Magic Powder - 1 (Codeforces): https://codeforces.com/problemset/problem/670/D1
+// ANNOTATED CODE
+// Naive Approach #2 (Can transition to binary search from here)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pi;
+const int MM = 1e6+3;
+int N, K, a[MM], b[MM], q[MM];
+int main() {
+    cin >> N >> K;
+    for (int i = 1; i <= N; i++) cin >> a[i];
+    for (int i = 1; i <= N; i++) cin >> b[i];
+    int ans = 0, req = 0; // req represents # of grams of special powder to increment ans (to create a new cookie)
+    while (true) {
+        for (int i = 1; i <= N; i++) {
+            if (b[i] >= a[i]) b[i] -= a[i];
+            else {
+                req += (a[i] - b[i]);
+                b[i] = 0;
+            }
+        }
+        if (req <= K) {
+            K -= req; ans++;
+        }
+        else break;
+        req = 0;
+    }
+    cout << ans << endl;
+}
