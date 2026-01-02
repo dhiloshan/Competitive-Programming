6f92397f074c60886f235682e0bc9c976a13cf12
Dhiloshan Sayanthan <dhiloshan@gmail.com>
2026-01-01 22:24:35 -0500

    Create Codeforces-B-Interesting-Drink-R367-Div2(Raw).cpp

---
+++ Implementation/Binary-Search/Codeforces-B-Interesting-Drink-R367-Div2(Raw).cpp
@@ -0,0 +1,23 @@
+// B. Interesting drink (Codeforces): https://codeforces.com/problemset/problem/706/B/
+// RAW CODE
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int MM = 2e5+3;
+int N, Q, a[MM];
+int main() {
+    cin >> N;
+    for (int i = 1; i <= N; i++) cin >> a[i];
+    sort(a+1, a+N+1);
+    cin >> Q;
+    for (int i = 1, k; i <= Q; i++){
+        cin >> k;
+        int lo = 0, hi = N+1;
+        while (hi - lo > 1) {
+            int mi = lo + (hi-lo)/2;
+            if (a[mi] <= k) lo = mi;
+            else hi = mi;
+        }
+        cout << lo << endl;
+    }
+}
