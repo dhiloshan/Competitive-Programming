17fbfdd8920813a74d75d94a94d04b549897810c
Dhiloshan Sayanthan <dhiloshan@gmail.com>
2026-01-02 13:48:49 -0500

    Create Codeforces-C-Another-Problem-on-Strings-Div2-R112(Raw).cpp

---
+++ Implementation/Binary-Search/Codeforces-C-Another-Problem-on-Strings-Div2-R112(Raw).cpp
@@ -0,0 +1,38 @@
+// C. Another Problem on Strings (Codeforces): https://codeforces.com/problemset/problem/165/C
+// RAW CODE
+#include <bits/stdc++.h>
+using namespace std;
+const int MM = 1e6+3;
+typedef long long ll;
+int K, psa[MM]; string s; ll ans;
+int main() {
+    cin >> K >> s;
+    for (int i = 1; i <= s.length(); i++) {
+        if (s[i-1] == '1') {
+            psa[i]++;
+        }
+        psa[i] += psa[i-1];
+    }
+    for (int i = 1; i <= s.length(); i++) {
+        int lo = i-1, hi = s.length()+1;
+        while (hi - lo > 1) {
+            int mi = lo + (hi - lo) / 2;
+            int numOfOnes = psa[mi] - psa[i-1];
+            if (numOfOnes >= K) hi = mi;
+            else lo = mi;
+        }
+        int lb = hi;
+        if (hi != s.length()+1) {
+            lo = i-1; hi = s.length()+1;
+            while (hi - lo > 1) {
+                int mi = lo + (hi - lo) / 2;
+                int numOfOnes = psa[mi] - psa[i-1];
+                if (numOfOnes > K) hi = mi;
+                else lo = mi;
+            }
+            int ub = hi;
+            ans += ub - lb;
+        }
+    }
+    cout << ans << endl;
+}
